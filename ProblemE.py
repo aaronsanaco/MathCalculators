5c8e7747ad116023eac5b8e4d9b967833e66387f
lionad07 <lionalouise.dagoro@gmail.com>
2025-03-26 17:56:28 +0800

    Add `ProblemE.py` program.

---
+++ ProblemE.py
@@ -0,0 +1,17 @@
+# E. Slope of a Road: A road has a rise of 4 meters over a horizontal distance
+# of 100 meters. What is the slope of the road (in percentage)?
+# Formula: 𝑆 = (𝑅𝑖𝑠𝑒 / 𝑅𝑢𝑛) * 100
+# Output message: So, the slope of the road is S%.
+
+def main():
+    
+    Rise = float(input("Enter the rise of the road needed (m): "))
+    Run = float(input("Enter the run of the road needed (m): "))
+    
+    𝑆 = (𝑅𝑖𝑠𝑒 / 𝑅𝑢𝑛) * 100
+    
+    print(f"So, the slope of the road is {S:.2f} %.")
+    
+if __name__ == "__main__":
+    main()
+    
